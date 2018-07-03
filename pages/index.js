376f3ad45035944ec04bda42469eff99dd7a490c
Miguel Ángel Durán <miduga@gmail.com>
2018-07-03 14:41:38 +0200

    Fix homePage

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,6 @@ import {Separator} from '../components/Separator'
 import {CostAndValue} from '../components/CostAndValue'
 import {Team} from '../components/Team'
 
-import {Section} from '../components/Section'
-
 const Index = () => (
   <PageLayout
     description="Code consultancy studio. We help you creating your next MVP or project with care, focusing on quality and endurability. Best-in-class projects based on Javascript, ReactJS, GraphQL and Node."
