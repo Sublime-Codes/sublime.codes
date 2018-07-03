28fdfe0f3e76506923d193200137393f62336d88
Miguel Ángel Durán <miduga@gmail.com>
2018-07-03 14:35:57 +0200

    Create reusable logo

---
+++ components/Logo/index.js
@@ -0,0 +1,34 @@
+import Typist from 'react-typist'
+
+export const Logo = () => (
+  <React.Fragment>
+    <Typist startDelay={100}>sublime.codes</Typist>
+    <style jsx global>
+      {`
+        .Typist {
+          display: inline-block;
+        }
+        .Typist .Cursor {
+          display: inline-block;
+        }
+
+        .Typist .Cursor--blinking {
+          opacity: 1;
+          animation: blink 1s linear infinite;
+        }
+
+        @keyframes blink {
+          0% {
+            opacity: 1;
+          }
+          50% {
+            opacity: 0;
+          }
+          100% {
+            opacity: 1;
+          }
+        }
+      `}
+    </style>
+  </React.Fragment>
+)
