14bc97289a5b7ce92c1a54f695f3d738f5e4256e
Miguel Ángel Durán <miduga@gmail.com>
2018-07-03 14:35:32 +0200

    Create button to be reused

---
+++ components/Button/index.js
@@ -0,0 +1,32 @@
+export const Button = ({children, onClick}) => (
+  <React.Fragment>
+    <button onClick={onClick}>{children}</button>
+    <style jsx>{`
+      button {
+        align-items: center;
+        background: #1a73e8;
+        border: 0;
+        border-radius: 4px;
+        color: #fff;
+        cursor: pointer;
+        display: inline-flex;
+        font-size: 16px;
+        font-weight: 500;
+        height: 40px;
+        justify-content: center;
+        line-height: 0;
+        outline: none;
+        overflow: hidden;
+        padding: 0px 32px;
+        position: relative;
+        text-align: center;
+        transition: background 0.2s ease-out;
+        user-select: none;
+        white-space: nowrap;
+      }
+      button:hover {
+        background: #09f;
+      }
+    `}</style>
+  </React.Fragment>
+)
